741fa7d09c778798dae44b368ca909ac6d75c90d
RochevYaroslav <justmisterstone@gmail.com>
2025-09-29 17:13:40 +0500

    Task5

---
+++ Tyuiu.RochevYA.Sprint3.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.RochevYA.Sprint3.Task5.V9.Lib;
+namespace Tyuiu.RochevYA.Sprint3.Task5.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(1935, ds.GetSumSumSeries(5,1,3,1,14));
+        }
+    }
+}
---
+++ Tyuiu.RochevYA.Sprint3.Task5.V9/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.RochevYA.Sprint3.Task5.V9.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int x = 5;
+        int sta1 = 1;
+        int end1 = 3;
+        int sta2 = 1;
+        int end2 = 14;
+        Console.WriteLine(ds.GetSumSumSeries(x,sta1,sta2,end1, end2));
+        Console.WriteLine("Сделал Рочев Ярослав Антонович | ИБКСб-25-1");
+    }
+}
