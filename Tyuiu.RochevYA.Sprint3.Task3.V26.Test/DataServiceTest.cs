a5b41db059f53ecd2a645b0df3fdb8a8e259c8fa
RochevYaroslav <justmisterstone@gmail.com>
2025-09-29 16:56:31 +0500

    Task3

---
+++ Tyuiu.RochevYA.Sprint3.Task3.V26.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.RochevYA.Sprint3.Task3.V26.Lib;
+
+namespace Tyuiu.RochevYA.Sprint3.Task3.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(1935, ds.GetCharCount("hello",'e'));
+        }
+    }
+}
---
+++ Tyuiu.RochevYA.Sprint3.Task3.V26/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.RochevYA.Sprint3.Task3.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        char chor = 'e';
+        string text = "have a nice time";
+        Console.WriteLine(ds.GetCharCount(text,chor));
+        Console.WriteLine("Сделал Рочев Ярослав Антонович | ИБКСб-25-1");
+    }
+}
