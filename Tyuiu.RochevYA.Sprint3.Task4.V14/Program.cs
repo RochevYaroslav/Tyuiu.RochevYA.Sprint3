91fc13e0c524048fcda23523317de902aa9b6be6
RochevYaroslav <justmisterstone@gmail.com>
2025-09-29 17:04:40 +0500

    Task4

---
+++ Tyuiu.RochevYA.Sprint3.Task4.V14/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.RochevYA.Sprint3.Task4.V14.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int start = -5;
+        int end = 5;
+        Console.WriteLine(ds.Calculate(start, end));
+        Console.WriteLine("Сделал Рочев Ярослав Антонович | ИБКСб-25-1");
+    }
+}
